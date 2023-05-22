8463b77f068e29ae84d5a0c9ebc02b71fb168761
kimhyuna <hyunaa514@naver.com>
2023-05-22 11:27:43 +0900

    예치금환불/환불요청내역 페이지

--- src/views/dist/Refund.vue
+++ src/views/dist/Refund.vue
@@ -1,3 +1,27 @@
 <template>
-    <div> 환불1 </div>
+  <div class="refund-page">
+    <div class="container">
+      <h2 class="text-h2">{{ $route.name }}</h2>
+      <div>
+        <h4 class="text-h4">계좌 정보</h4>
+        <p class="font-weight-bold" style="font-size: 1.6rem;">카카오뱅크 79606702902101 예자인</p>
+      </div>
+      <div>
+        <h4 class="text-h4">예치금 잔액</h4>
+        <p class="font-weight-bolder primary--text text-h6">7,200</p>
+      </div>
+      <div>
+        <h4 class="text-h4">환불 요청 금액</h4>
+        <v-text-field placeholder="숫자만 입력해 주세요." outlined hide-details="auto"></v-text-field>
+      </div>
+      <div>
+        <h4 class="text-h4">환불 사유</h4>
+        <v-textarea placeholder="환불 사유를 입력해 주세요" outlined hide-details="auto" no-resize rows="10"></v-textarea>
+      </div>
+      <v-btn flat color="primary" x-large block>환불 요청</v-btn>
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+  @import '~@/assets/scss/page/charge/charge.scss';
+</style>
--- src/views/dist/RefundHistory.vue
+++ src/views/dist/RefundHistory.vue
@@ -1,3 +1,142 @@
 <template>
-    <div> 환불2 </div>
+  <div class="chargehis-page">
+    <div class="route-title">
+      <div class="container">
+        <h2 class="text-h2">{{ $route.name }}</h2>
+      </div>
+    </div>
+    <div class="chargehis-input">
+      <div class="container">
+        <div class="input-date d-flex">
+          <p>기간</p>
+          <div class="d-flex align-center">
+            <div>
+              <v-menu v-model="menu" :close-on-content-click="false" transition="scale-transition" offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                  <!-- YY-MM-DD형식 -->
+                  <v-text-field v-model="s_date" v-bind="attrs" outlined hide-details="auto" v-on="on"
+                    prepend-inner-icon="fa-"></v-text-field>
+                </template>
+                <v-date-picker v-model="s_date" no-title @input="menu = false"></v-date-picker>
+              </v-menu>
+            </div>
+            <p style="margin:0 5px">~</p>
+            <div>
+              <v-menu v-model="menu2" :close-on-content-click="false" transition="scale-transition" offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field v-model="e_date" v-bind="attrs" outlined hide-details="auto" v-on="on"
+                    prepend-inner-icon="fa-" type="number"></v-text-field>
+                </template>
+                <v-date-picker v-model="e_date" no-title @input="menu2 = false"></v-date-picker>
+              </v-menu>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex">
+          <p>환불번호</p>
+          <v-text-field placeholder="숫자만 입력해 주세요." outlined hide-details="auto"></v-text-field>
+        </div>
+        <div class="d-flex">
+          <p>진행상황</p>
+          <v-select v-model="selected" :items="items" outlined hide-details="auto" dense :menu-props="{ offsetY: true }"></v-select>
+        </div>
+        <div>
+          <v-btn flat color="primary">조회</v-btn>
+        </div>
+      </div>
+    </div>
+    <div class="list-table-wrap">
+      <div>
+        <v-data-table :headers="headers" :items="contents" :items-per-page="itemsPerPage" hide-default-footer
+          :mobile-breakpoint="0" :class="$vuetify.breakpoint.xs?'mobile-table':''">
+        </v-data-table>
+        <v-pagination v-model="page" :length="15" :total-visible="7" circle active-color="#399FC7"></v-pagination>
+      </div>
+    </div>
+  </div>
 </template>
+<script>
+  export default {
+    components: {},
+    data() {
+      return {
+        s_date: new Date().toISOString().substr(0, 10),
+        e_date: new Date().toISOString().substr(0, 10),
+        page: 1,
+        headers: [{
+          text: '순번',
+          value: 'num',
+          sortable: false,
+          width: '7%'
+        },
+        {
+          text: '환불요청번호',
+          value: 'num1',
+          sortable: false,
+          width: '10%',
+        },
+        {
+          text: '요청금액',
+          value: 'num2',
+          sortable: false,
+          width: '10%'
+        },
+        {
+          text: '사유',
+          value: 'txt',
+          sortable: false,
+          width: '20%'
+        },
+        {
+          text: '결과사유',
+          value: 'txt2',
+          sortable: false,
+          width: '20%'
+        },
+        {
+          text: '진행상황',
+          value: 'txt3',
+          sortable: false,
+          width: '10%'
+        },
+        {
+          text: '변경일',
+          value: 'date',
+          sortable: false,
+          width: '10%'
+        },
+        ],
+        contents: [{
+          num: '1',
+          num1: '01012341234',
+          num2: '3,000',
+          txt: '-',
+          txt2: '-',
+          txt3: '13,300',
+          date: '23.05.12'
+        },
+        {
+          num: '2',
+          num1: '01012341234',
+          num2: '3,000',
+          txt: '-',
+          txt2: '-',
+          txt3: '13,300',
+          date: '23.05.12'
+        }
+        ],
+        selected: '전체',
+        items: ['전체', '전체2', '전체3'],
+      }
+    },
+
+    computed: {
+      pageCount() {
+        return Math.ceil(this.contents.length / this.itemsPerPage)
+      },
+    },
+  }
+</script>
+<style lang="scss" scoped>
+  @import '~@/assets/scss/page/charge/charge.scss';
+</style>
