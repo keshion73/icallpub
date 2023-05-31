2b2ec28d37d6ede12a5f971e3527f36e84162cb0
kimhyuna <hyunaa514@naver.com>
2023-05-31 13:02:07 +0900

    충전 디자인 수정

--- src/views/dist/Charge.vue
+++ src/views/dist/Charge.vue
@@ -1,93 +1,126 @@
 <template>
-  <div class="charge-page d-flex">
+  <div class="charge-page">
     <Loading>
       <p>충전 중 입니다.</p>
     </Loading>
-    <div class="container">
-        <h2 class="text-h2">{{ $route.name }}</h2>
-        <v-form v-if="!$vuetify.breakpoint.xs">
-          <div class="charge-info">
-            <h4 class="text-h4">예치금 잔액</h4>
-             <p><span>7,200</span><span>원</span></p>
+    <div class="charge-cont">
+      <div class="container noti-cont" v-if="!$vuetify.breakpoint.xs">
+        <div class="charge-noti">
+          <h4 class="text-h4">충전시 유의사항</h4>
+          <p>
+            타사 충전 문의 : 02.811.3729<br>운영시간 : 오전 10시 - 오후 9시<br><br>
+            전용계좌 2만원부터 입금가능<br>(입금시 1분 소요, 재 로그인 필수)<br>
+            회사에따라 충전 시간이 다를 수 있습니다.<br><br>
+            모빙 정액요금제 충전 불가<br>
+            일반요금제 충전 취소 불가<br><br>
+            충전 취소는 상황에따라 불가 할 수 있으며, <br>
+            취소 시 시간이 다소 걸릴수 있습니다.
+          </p>
+        </div>
+      </div>
+      <h2 class="text-h2">{{ $route.name }}</h2>
+      <v-form v-if="!$vuetify.breakpoint.xs">
+        <div class="charge-info">
+          <h4 class="text-h4">예치금 잔액</h4>
+          <p><span>7,200</span><span>원</span></p>
+        </div>
+        <div>
+          <h4 class="text-h4">폰번호</h4>
+          <div class="input-ph">
+            <v-text-field placeholder="휴대폰번호" outlined></v-text-field>
+            <v-btn color="primary">확인</v-btn>
+          </div>
+        </div>
+        <div class="charge-bg">
+          <div>
+            <h4 class="text-h4">고객정보</h4>
+            <div class="d-flex"><span class="mark">스마트앱</span>
+              <p>SKT PPS Premium</p>
+              <span class="mark mark-oth"><img src="@/assets/images/common/icon-infored.svg" alt="">타사 상품</span>
+            </div>
           </div>
           <div>
-            <h4 class="text-h4">폰번호</h4>
-            <div class="input-ph">
-              <v-text-field placeholder="휴대폰번호" outlined></v-text-field>
-              <v-btn color="primary">확인</v-btn>
-            </div>
+            <h4 class="text-h4">충전 잔액</h4>
+            <v-radio-group v-model="radio">
+              <v-radio label="10,000" value="1" checked></v-radio>
+              <v-radio label="20,000" value="2"></v-radio>
+              <v-radio label="30,000" value="3"></v-radio>
+              <v-radio label="40,000" value="4"></v-radio>
+              <v-radio label="50,000" value="5"></v-radio>
+            </v-radio-group>
           </div>
+          <div>
+            <h4 class="text-h4">예치금 차감액</h4>
+            <p class="font-weight-bolder" style="font-size: 2rem;color:#666">0</p>
+            <p class="info-mes">예치금 차감액은 10% 차익이 있으며, 궁금한 사항은 아이콜 모바일 본사로 연락주시기 바랍니다.</p>
+          </div>
+          <v-btn color="btncolor" x-large block>충전</v-btn>
+        </div>
+      </v-form>      
+      <div v-if="$vuetify.breakpoint.xs">
+        <v-form action="">
+          <div class="charge-info">
+          <h4 class="text-h4">예치금 잔액</h4>
+          <p><span>7,200</span><span>원</span></p>
+        </div>
+        <div>
+          <h4 class="text-h4">폰번호</h4>
+          <div class="input-ph">
+            <v-text-field placeholder="휴대폰번호" outlined></v-text-field>
+            <v-btn color="primary">확인</v-btn>
+          </div>
+        </div>
           <div class="charge-bg">
-            <div>
-              <h4 class="text-h4">예치금 잔액</h4>
-              <p>SKT PPS Premium</p>
-            </div>
-            <div>
-              <h4 class="text-h4">충전 잔액</h4>
-              <v-radio-group v-model="radio">
-                <v-radio label="10,000" value="1" checked></v-radio>
-                <v-radio label="20,000" value="2"></v-radio>
-                <v-radio label="30,000" value="3"></v-radio>
-                <v-radio label="40,000" value="4"></v-radio>
-                <v-radio label="50,000" value="5"></v-radio>
-              </v-radio-group>
-            </div>
-            <div>
-              <h4 class="text-h4">예치금 차감액</h4>
-              <p class="font-weight-bolder">0</p>
-              <p class="info-mes">예치금 차감액은 10% 차익이 있으며, 궁금한 사항은 아이콜 모바일 본사로 연락주시기 바랍니다.</p>
-            </div>
-            <v-btn color="btncolor" x-large block>충전</v-btn>
-          </div>
-        </v-form>
-        <div v-if="$vuetify.breakpoint.xs">
-          <v-form action="">
-            <div>
-              <h4 class="text-h4">예치금 잔액</h4>
-              <p class="font-weight-bolder primary--text text-h6">7,200</p>
-            </div>
-            <div>
-              <h4 class="text-h4">폰번호</h4>
-              <div class="input-ph">
-                <v-text-field placeholder="휴대폰번호" outlined></v-text-field>
-                <v-btn color="btncolor">확인</v-btn>
-              </div>
-            </div>
-            <div>
-              <h4 class="text-h4">고객정보</h4>
-              <!--입력전-->
+          <div>
+            <h4 class="text-h4">고객정보</h4>
+            <!--입력전-->
               <p style="color:#AAB5C9" class="info-mes">폰번호 입력 후 확인을 눌러주세요.</p>
-              <p>LG 선불 스마트 300M</p>
+            <div class="d-flex">
+              <span class="mark">스마트앱</span>
+              <p>SKT PPS Premium</p>
+              <span class="mark mark-oth"><img src="@/assets/images/common/icon-infored.svg" alt="">타사 상품</span>
             </div>
-            <div>
-              <h4 class="text-h4">충전금액</h4>
-              <!--입력전-->
-              <p style="color:#AAB5C9" class="info-mes">일반 요금제는 선택, 선불 요금제는 자동입력됩니다.</p>
-              <!--선불요금제일시-->
+          </div>
+          <div>
+            <h4 class="text-h4">충전금액</h4>
+            <!--입력전-->
+            <p style="color:#AAB5C9" class="info-mes">일반 요금제는 선택, 선불 요금제는 자동입력됩니다.</p>
+            <!--선불요금제일시-->
               <p class="font-weight-bolder primary--text text-h6">36,000</p>
               <!--일반요금제일시-->
-              <div>
-                <v-radio-group v-model="radio">
-                  <v-radio label="10,000" value="1" checked></v-radio>
-                  <v-radio label="20,000" value="2"></v-radio>
-                  <v-radio label="30,000" value="3"></v-radio>
-                  <v-radio label="40,000" value="4"></v-radio>
-                  <v-radio label="50,000" value="5"></v-radio>
-                </v-radio-group>
-              </div>
-            </div>
-            <!--입력후-->
-            <div>
-              <div>
-                <h4 class="text-h4">예치금 차감액</h4>
-                <p class="font-weight-bolder">32,400</p>
-                <p class="info-mes">예치금 차감액은 10% 차익이 있으며, 궁금한 사항은 아이콜 모바일 본사로 연락주시기 바랍니다.</p>
-              </div>
-            </div>
-            <!--입력전 disabled-->
-            <v-btn color="btncolor" x-large block>충전</v-btn>
-          </v-form>
+            <v-radio-group v-model="radio">
+              <v-radio label="10,000" value="1" checked></v-radio>
+              <v-radio label="20,000" value="2"></v-radio>
+              <v-radio label="30,000" value="3"></v-radio>
+              <v-radio label="40,000" value="4"></v-radio>
+              <v-radio label="50,000" value="5"></v-radio>
+            </v-radio-group>
+          </div>
+          <div>
+            <!--입력후 노출-->
+            <h4 class="text-h4">예치금 차감액</h4>
+            <p class="font-weight-bolder" style="font-size: 2rem;color:#666">0</p>
+            <p class="info-mes">예치금 차감액은 10% 차익이 있으며, 궁금한 사항은 아이콜 모바일 본사로 연락주시기 바랍니다.</p>
+          </div>
+          <!--입력전 disabled-->
+          <v-btn color="btncolor" x-large block>충전</v-btn>
+          <div class="container noti-cont">
+        <div class="charge-noti">
+          <h4 class="text-h4">충전시 유의사항</h4>
+          <p>
+            타사 충전 문의 : 02.811.3729<br>운영시간 : 오전 10시 - 오후 9시<br><br>
+            전용계좌 2만원부터 입금가능<br>(입금시 1분 소요, 재 로그인 필수)<br>
+            회사에따라 충전 시간이 다를 수 있습니다.<br><br>
+            모빙 정액요금제 충전 불가<br>
+            일반요금제 충전 취소 불가<br><br>
+            충전 취소는 상황에따라 불가 할 수 있으며, <br>
+            취소 시 시간이 다소 걸릴수 있습니다.
+          </p>
         </div>
+      </div>
+        </div>
+        </v-form>
+      </div>
       <div @click="dialog = true" class="btn-sales">
         <img src="@/assets/images/page/charge/btn-sales.png" alt="">
       </div>
@@ -105,7 +138,7 @@
             <v-window-item :transition="false">
               <div>
                 <v-data-table :headers="headers" :items="arr1" hide-default-footer no-data-text="데이터가 없습니다."
-                  disable-pagination>
+                  disable-pagination mobile-breakpoint="0">
                   <template #[`item.sales_discount_rate`]="{ item }">
                     <span>
                       {{ item.sales_discount_rate }}%
@@ -117,7 +150,7 @@
             <v-window-item :transition="false">
               <div>
                 <v-data-table :headers="headers" :items="arr2" hide-default-footer no-data-text="데이터가 없습니다."
-                  disable-pagination>
+                  disable-pagination mobile-breakpoint="0">
                   <template #[`item.sales_discount_rate`]="{ item }">
                     <span>
                       {{ item.sales_discount_rate }}%
@@ -134,9 +167,10 @@
 </template>
 <script>
   import Loading from '../../components/Loading'
-export default {
+  export default {
     components: {
-    Loading,},
+      Loading,
+    },
     data() {
       return {
         tab: null,
@@ -333,6 +367,7 @@ export default {
 </script>
 <style lang="scss" scoped>
   @import '~@/assets/scss/page/charge/charge.scss';
+
   ::v-deep {
     .v-data-table {
       height: 560px;
@@ -341,7 +376,7 @@ export default {
       scrollbar-color: #00648A #ffffff;
 
       .v-data-table__wrapper {
-        width: 300px;
+        width: 90%;
         overflow: hidden;
       }
 
